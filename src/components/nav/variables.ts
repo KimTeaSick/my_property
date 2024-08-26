cd80b1f27cdfb745935ce4955a4fa980ee98aa4e
kimjoseph <notseph8556@gmail.com>
2024-08-26 16:03:22 +0900

    nav added

--- src/components/nav/variables.ts
+++ src/components/nav/variables.ts
@@ -2,5 +2,6 @@ export const NAV_LIST = [
   { label: "main", goal: "/" },
   { label: "coin", goal: "/coin" },
   { label: "condition", goal: "/condition" },
+  { label: "trading", goal: "/trading" },
   { label: "auto trading", goal: "/autotrading" },
 ];
